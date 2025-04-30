63e2ba8d3dd622a5028527b4e9a4617ad450913f
Telha Wasim <telhawasim@gmail.com>
2025-04-30 12:54:35 +0500

    removed uncessary files

--- components/appDivider.tsx
+++
@@ -1,8 +0,0 @@
-import { View, Text } from "react-native";
-import React from "react";
-
-const AppDivider = () => {
-  return <View className="w-full h-[1] bg-divider"></View>;
-};
-
-export default AppDivider;
